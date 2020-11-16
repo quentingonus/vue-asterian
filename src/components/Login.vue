653f1dc1a4170692473f4b0c92326dc8deee453c
quentingonus <55360738+quentingonus@users.noreply.github.com>
2020-11-17 00:20:04 +0630

    first

---
+++ src/components/Login.vue
@@ -0,0 +1,113 @@
+<template>
+  <v-container>
+    <v-row no-gutters>
+      <v-spacer></v-spacer>
+      <v-card>
+        <v-card-title>Create an Account</v-card-title>
+        <v-card-text>
+          <v-stepper v-model="e1">
+            <v-stepper-header>
+              <v-stepper-step :complete="e1 > 1" step="1">
+                Name
+              </v-stepper-step>
+
+              <v-divider></v-divider>
+
+              <v-stepper-step :complete="e1 > 2" step="2">
+                Security
+              </v-stepper-step>
+
+              <v-divider></v-divider>
+
+              <v-stepper-step step="3">
+                Additional Info
+              </v-stepper-step>
+            </v-stepper-header>
+
+            <v-stepper-items>
+              <v-stepper-content step="1">
+                <v-form v-model="valid">
+                  <v-container>
+                    <v-row>
+                      <v-col cols="12" md="4">
+                        <v-text-field
+                          v-model="firstname"
+                          :rules="nameRules"
+                          :counter="10"
+                          label="First name"
+                          required
+                        ></v-text-field>
+                      </v-col>
+
+                      <v-col cols="12" md="4">
+                        <v-text-field
+                          v-model="lastname"
+                          :rules="nameRules"
+                          :counter="10"
+                          label="Last name"
+                          required
+                        ></v-text-field>
+                      </v-col>
+
+                      <v-col cols="12" md="4">
+                        <v-text-field
+                          v-model="email"
+                          :rules="emailRules"
+                          label="E-mail"
+                          required
+                        ></v-text-field>
+                      </v-col>
+                    </v-row>
+                  </v-container>
+                </v-form>
+
+                <v-btn color="primary" @click="e1 = 2">
+                  Continue
+                </v-btn>
+
+                <v-btn text>
+                  Cancel
+                </v-btn>
+              </v-stepper-content>
+
+              <v-stepper-content step="2">
+                <v-card class="mb-12" color="grey lighten-1" height="200px"></v-card>
+
+                <v-btn color="primary" @click="e1 = 3">
+                  Continue
+                </v-btn>
+
+                <v-btn text>
+                  Cancel
+                </v-btn>
+              </v-stepper-content>
+
+              <v-stepper-content step="3">
+                <v-card class="mb-12" color="grey lighten-1" height="200px"></v-card>
+
+                <v-btn color="primary" @click="e1 = 1">
+                  Continue
+                </v-btn>
+
+                <v-btn text>
+                  Cancel
+                </v-btn>
+              </v-stepper-content>
+            </v-stepper-items>
+          </v-stepper>
+        </v-card-text>
+        <v-card-actions> </v-card-actions>
+      </v-card>
+      <v-spacer></v-spacer>
+    </v-row>
+  </v-container>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      e1: 1
+    };
+  }
+};
+</script>
---
+++ src/components/Nav.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-card>
+    <v-app-bar absolute elevate-on-scroll scroll-target="#scrolling-techniques-7">
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Asterian</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        icon
+        transition="scale-transition"
+        :loading="loading"
+        @click="change_modes"
+        class="mr-5"
+        ><v-icon>{{ day_night }}</v-icon></v-btn
+      >
+    </v-app-bar>
+  </v-card>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      loading: false
+    };
+  },
+  computed: {
+    day_night() {
+      this.make_loading();
+      return this.$vuetify.theme.dark ? "mdi-moon-waxing-crescent" : "mdi-white-balance-sunny";
+    }
+  },
+  methods: {
+    change_modes() {
+      this.$vuetify.theme.dark = !this.$vuetify.theme.dark;
+    },
+    make_loading() {
+      this.loading = true;
+      setTimeout(() => {
+        this.loading = !this.loading;
+      }, Math.floor(Math.random() * 500) + 1);
+    }
+  }
+};
+</script>
